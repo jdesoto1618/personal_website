336238f43edb83d88fbc0e992dfbf720f75381af
Joe DeSoto <modulusman@gmail.com>
2017-09-24 10:19:01 -0700

    removes extra js file, adds comments in main js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,30 @@ $('.menu-container').on('click', function(){
   $('.list-inline').toggleClass('open');
 });
 
-// menu bars
+// add class of change to menu-container div when it's clicked
 function menuCollapse(x) {
   x.classList.toggle('change');
 }
+
+// image slider code
+document.addEventListener('DOMContentLoaded', function(){
+  let wrapper = document.getElementById('wrapper');
+  let topLayer = wrapper.querySelector('.top');
+  let handle = wrapper.querySelector('.handle');
+  let skew = 0;
+  let delta = 0;
+
+  if(wrapper.className.indexOf('skewed') != -1) {
+    skew = 1000;
+  }
+
+  wrapper.addEventListener('mousemove', function (e){
+    delta = (e.clientX - window.innerWidth / 2) * 0.5;
+    handle.style.left = e.clientX + delta + 'px';
+    topLayer.style.width = e.clientX + skew + delta + 'px';
+  });
+});
+// end image slider code
 
 // Get the modal
 var modal = document.getElementById('myModal');
