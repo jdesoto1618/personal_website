6d035d289f654da683c5f079f3977543009ed54f
Joe <modulusman@gmail.com>
2020-06-27 12:26:03 -0700

    Updates app.js to remoe unused code for targeting input fields.
    Also removes second check for com loaded in app.js.
    
    Sets guard clause for year to prevent console error if the
    date span is not present or has a different class name.

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let navList = getContainers()[1];
   let imgContainer = getContainers()[2];
   let modalContainer = getContainers()[3];
+  const bodyTag = document.querySelector('body');
 
   menuContainer.addEventListener('click', function(){
     menuContainer.classList.toggle('update-menu-bars');
@@ -18,30 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   let yearText = document.querySelector('.current-year');
-  yearText.innerHTML = new Date().getFullYear();
+  if(yearText) {
+    yearText.innerHTML = new Date().getFullYear();
+  }
 
-  // let inputUnderline = document.querySelector('.input-underline');
-  // let emailInput = document.querySelector('.email-input');
-  //
-  // emailInput.addEventListener('focusin', function(){
-  //   inputUnderline.classList.add('show-input-underline');
-  // });
-  //
-  // emailInput.addEventListener('focusout', function(){
-  //   inputUnderline.classList.remove('show-input-underline');
-  // });
-
-  if ($('body').hasClass('index')) {
-    // multi-image modal
-    $(document).ready(function () {
-      $('#myModal').on('show.bs.modal', function (e) {
-        var image = $(e.relatedTarget).attr('src');
-        $('.showimage').attr('src', image);
-      });
+  if (bodyTag.classList.contains('home')) {
+    $('#myModal').on('show.bs.modal', function (e) {
+      var image = $(e.relatedTarget).attr('src');
+      $('.showimage').attr('src', image);
     });
-
-  } else if ($('body').hasClass('projects')) {
-    // image slider code
+  } else if (bodyTag.classList.contains('projects')) {
     let wrapper = document.getElementById('wrapper');
     let topLayer = wrapper.querySelector('.top');
     let handle = wrapper.querySelector('.handle');
@@ -56,7 +43,6 @@ document.addEventListener('DOMContentLoaded', function() {
       delta = (e.clientX - window.innerWidth / 2) * 0.5;
       handle.style.left = e.clientX + delta + 'px';
       topLayer.style.width = e.clientX + skew + delta + 'px';
-      // end image slider code
-    }); //end wrapper function
-  } // end else if
+    });
+  }
 });
