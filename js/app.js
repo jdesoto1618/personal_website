3264547447c62cc464305e140d03b72d585a2cc2
Joe <modulusman@gmail.com>
2020-06-28 11:01:49 -0700

    Repairs projects page and image slider.
    Also sets this to appear at 1024px and up, and
    be removed from the page at anything less than that.

--- js/app.js
+++ js/app.js
@@ -29,20 +29,18 @@ document.addEventListener('DOMContentLoaded', function() {
       $('.showimage').attr('src', image);
     });
   } else if (bodyTag.classList.contains('projects')) {
-    let wrapper = document.getElementById('wrapper');
+    let wrapper = document.querySelector('#wrapper');
     let topLayer = wrapper.querySelector('.top');
     let handle = wrapper.querySelector('.handle');
     let skew = 0;
-    let delta = 0;
 
-    if(wrapper.className.indexOf('skewed') != -1) {
-      skew = 1000;
+    if(wrapper.classList.contains('skewed')) {
+      skew = 192.5;
     }
 
     wrapper.addEventListener('mousemove', function (e) {
-      delta = (e.clientX - window.innerWidth / 2) * 0.5;
-      handle.style.left = e.clientX + delta + 'px';
-      topLayer.style.width = e.clientX + skew + delta + 'px';
+      handle.style.left = e.clientX + 'px';
+      topLayer.style.width = e.clientX + skew + 'px';
     });
   }
 });
